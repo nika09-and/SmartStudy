cd147887d6e040c2e4866f08c770f52d2544209e
nika09-and <andriadze.n1@gmail.com>
2026-02-12 15:35:56 +0400

    create page changes
    create page changes

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,16 @@
 import styles from "./index.module.css";
 
-const Button = ({ text, color }) => {
+const Button = ({ text, color, height, width, fontSize }) => {
   return (
-    <button className={styles.button} style={{ backgroundColor: color }}>
+    <button
+      className={styles.button}
+      style={{
+        backgroundColor: color,
+        height: height,
+        width: width,
+        fontSize: fontSize,
+      }}
+    >
       {text}
     </button>
   );
